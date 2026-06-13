1ace82d75938486f994ea9f14addf62363554383
Edong <julbedong@gmail.com>
2026-06-14 01:03:30 +0800

    Update preprocessed data in MLProject

--- MLProject/modelling.py
+++ MLProject/modelling.py
@@ -7,28 +7,19 @@ import os
 import shutil
 
 def main():
-    # Gunakan mlflow tracking lokal (default ./mlruns)
     mlflow.set_experiment("Titanic_Classifier_Retrain")
 
-    # 1. Load dataset
-    print("Membaca dataset...")
     df = pd.read_csv("titanic_preprocessed.csv")
-
-    # 2. Pisahkan fitur dan target
     X = df.drop(columns=["Survived"])
     y = df["Survived"]
 
-    # 3. Train-test split
     X_train, X_test, y_train, y_test = train_test_split(
         X, y, test_size=0.2, random_state=42, stratify=y
     )
 
-    # 4. Aktifkan mlflow.autolog()
     mlflow.autolog()
 
-    # 5. Training Random Forest
-    print("Melatih model...")
-    with mlflow.start_run(run_name="RandomForest_Retrain") as run:
+    with mlflow.start_run(run_name="RandomForest_Retrain"):
         model = RandomForestClassifier(random_state=42)
         model.fit(X_train, y_train)
         
@@ -37,15 +28,12 @@ def main():
         print(f"Train Accuracy: {train_acc:.4f}")
         print(f"Test Accuracy: {test_acc:.4f}")
 
-        # Simpan model secara eksplisit ke folder artifacts/model untuk workflow CI
-        # Bersihkan folder jika sudah ada sebelumnya
         artifacts_dir = os.path.join("artifacts", "model")
         if os.path.exists(artifacts_dir):
             shutil.rmtree(artifacts_dir)
         
         os.makedirs(os.path.dirname(artifacts_dir), exist_ok=True)
         mlflow.sklearn.save_model(model, artifacts_dir)
-        print(f"Model berhasil dilatih dan disimpan secara lokal di: {artifacts_dir}")
 
 if __name__ == "__main__":
     main()
